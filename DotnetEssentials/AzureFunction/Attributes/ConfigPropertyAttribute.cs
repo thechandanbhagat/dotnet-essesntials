f8a2f9237b36bfe12a413ea9eb3fd1b276e1f48a
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2024-04-26 14:47:27 +0100

    Initial commit

---
+++ DotnetEssentials/AzureFunction/Attributes/ConfigPropertyAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace DotnetEssentials.AzureFunction.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public class ConfigPropertyAttribute : Attribute
+    {
+        private string configName;
+        public string ConfigName => configName;
+
+        public ConfigPropertyAttribute(string configName)
+        {
+            this.configName = configName;
+        }
+    }
+}
---
+++ DotnetEssentials/AzureFunction/Extension/ConfigurationExtension.cs
@@ -0,0 +1,29 @@
+﻿using DotnetEssentials.AzureFunction.Attributes;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetEssentials.AzureFunction.Extension
+{
+    public static class ConfigurationExtension
+    {
+        public static TModel LoadConfig<TModel>(this IConfiguration config)
+        {
+            var ret = default(TModel);
+            var props = ret.GetType().GetProperties();
+            foreach (var prop in props)
+            {
+                var attr = prop.GetCustomAttribute<ConfigPropertyAttribute>();
+                if (attr != null)
+                {
+                    prop.SetValue(ret, config[attr.ConfigName]);
+                }
+            }
+            return ret;
+        }
+    }
+}
