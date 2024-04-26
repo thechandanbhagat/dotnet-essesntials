6686ad55e47dc7ac52a50443136d8eced4dd7857
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2024-04-26 15:37:02 +0100

    chore: enumerables function extension added

---
+++ DotnetEssentials/Linq/Extension/EnumerableExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetEssentials.Linq.Extension
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEachItem<T>(this IEnumerable<T> enumerables, Action<T> action)
+        {
+            foreach (var item in enumerables)
+                action(item);
+        }
+
+        public static Task ForEachAsync<T>(this IEnumerable<T> enumerables, Func<T, Task> func) => Task.WhenAll(enumerables.Select(func).ToList());
+
+        public static TSource EmptyIfNull<TSource>(this TSource source) where TSource : class, IEnumerable, new() => source ?? new TSource();
+    }
+}
